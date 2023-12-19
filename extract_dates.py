07b39a408bad3e4ec37947c47ba0fab2c767dd69
jackson-hu1279 <68998854+jackson-hu1279@users.noreply.github.com>
2023-12-19 18:06:40 +1100

    Add script to check available dates

---
+++ extract_dates.py
@@ -0,0 +1,30 @@
+import requests
+import re
+import random
+import time
+
+custom_cookies = {}
+custom_cookies['.ASPXAUTH'] = ''
+custom_cookies['ASP.NET_SessionId'] = ''
+custom_cookies['AWSALB'] = ''
+custom_cookies['AWSALBCORS'] = ''
+custom_cookies['VisaBookingType'] = ''
+custom_cookies['lpTestCookie1702961817385'] = ''
+
+
+booking_url = "https://bmvs.onlineappointmentscheduling.net.au/oasis/AppointmentTime.aspx"
+
+def check_available_dates(custom_cookies, booking_url):
+    response = requests.get(url=booking_url, cookies=custom_cookies)
+
+    available_dates = re.search(r'var gAvailDates = \[(.*)\];', response.text).group(1)
+    print("Extracted HTML var:", available_dates)
+
+    date_lst = re.findall(r'new Date\((.*?)\)', available_dates)
+    if date_lst:
+        print("\nAvailable Dates:")
+        for date in date_lst:
+            print(date)
+
+
+check_available_dates(custom_cookies, booking_url)
